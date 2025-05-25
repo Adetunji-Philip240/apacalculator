4ee4daff990c18f333df2e873d7eb82920ae3373
Adetunji-Philip240 <adewalegithub240@gmail.com>
2025-05-25 02:14:03 +0100

    Updating the home page

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ window.onload = function () {
   setTimeout(hideLoader, 1000); // Adjust time as needed
 };
 //Loader END
+
+
+//Navbar
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  const toggler = document.getElementById("toggler");
+  menu.classList.toggle("active");
+  if (menu.classList.contains("active")) {
+    toggler.classList.remove("fa.bars");
+    toggler.classList.add("fa-times");
+  } else {
+    toggler.classList.remove("fa-times");
+    toggler.classList.add("fa-bars");
+  }
+}
