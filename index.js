6b4227b726900d6d2e8ac097ef6549606b8cf7d0
unknown <adewalegithub240@gmail.com>
2025-08-16 22:16:27 +0100

    Removing the loader

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 
-//Loader BEGIN
-
-// Function to simulate loading
-function hideLoader() {
-  const loader = document.querySelector(".loader");
-  const content = document.querySelector(".content");
-  const loaderbody = document.querySelector(".loaderbody");
-  loader.style.display = "none"; // Hide loader
-  loaderbody.style.display = "none"; // Hide loader body
-  content.style.display = "block"; // Show content
-}
-
-// Simulate a loading time (e.g., 2 seconds)
-window.onload = function () {
-  setTimeout(hideLoader, 1000); // Adjust time as needed
-};
-//Loader END
 
 
 //Navbar
